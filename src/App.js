6ba0267e196a6792a689aa281840b478cc098694
hendro19 <55833889+hendro19@users.noreply.github.com>
2020-09-04 01:58:00 +0700

    kompoonen

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
 import React from "react";
 import "./styles.css";
+import Paper from "./componets/paper";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color">add</button>
-          <h1 className="header-title">Todo List</h1>
-          <button className="header-btn main-red-color">Clear</button>
-        </section>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">add</button>
+        <h1 className="header-title">Todo List</h1>
+        <button className="header-btn main-red-color">Clear</button>
+      </section>
 
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input" />
-            <button className="add-btn main-black-color">Add</button>
-          </form>
-        </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
 
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Lering path</span>
-          </div>
-        </section>
-      </div>
-    </div>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Lering path</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
