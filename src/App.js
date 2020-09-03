83bcc5ceb01c586821b9d0aa2acd89c4fa536d72
hendro19 <55833889+hendro19@users.noreply.github.com>
2020-09-04 02:21:06 +0700

    header,todofrom,todos

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React from "react";
 import "./styles.css";
 import Paper from "./componets/paper";
+import Header from "./componets/header";
+import Header from "./componets/todofrom";
+import Header from "./componets/todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">add</button>
-        <h1 className="header-title">Todo List</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Lering path</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }
---
+++ src/componets/todos.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Lering path</span>
+      </div>
+    </section>
+  );
+};
+
+export default Todos;
