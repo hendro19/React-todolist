6c5e1a7100273780137891818631fc7d8d653c06
hendro19 <55833889+hendro19@users.noreply.github.com>
2020-09-16 22:13:10 +0700

    erorrr cuk

--- src/componets/todofrom.jsx
+++ src/componets/todofrom.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(value);
+  };
+
+  console.log("value".value);
   return (
     <section className="add">
       <form className="add-form">
-        <input type="text" className="add-input" />
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn main-black-color">Add</button>
       </form>
     </section>
